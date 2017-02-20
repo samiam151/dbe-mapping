5ed5bf0e4e35b13761a15ae62416ae590604aec3
Nicolas Sam <Nicolas Sam>
2017-02-19 23:52:38 -0500

    Some cleanup

--- js/script.js
+++ js/script.js
@@ -1,24 +1,25 @@
-window.initMap = function(){
+(function(window){
+    function _initMap(){
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 11,
+            center: {lat: 38.917, lng: -77.016420} // center of Wash.DC
+        });
 
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 11,
-        center: {lat: 38.910795, lng: -77.016420} // center of Wash.DC
-    });
+        $.get('data/data_fix.json').then(function(data){
+            data.forEach(function(business){
+                addMarker(business);
+            });      
+        });
 
-    $.get('data/data_fix.json').then(function(data){
-        data.forEach(function(business){
-            addMarker(business);
-        });      
-    });
-
-    function addMarker(point){
-        var coords = { lat: point.Coordinates.Latitude, lng:point.Coordinates.Longitude};
-        if (coords.lat && coords.lng){
-            new google.maps.Marker({
-                position: coords,
-                map: map 
-            });
-        }
-    } 
-
-}
+        function addMarker(point){
+            var coords = { lat: point.Coordinates.Latitude, lng:point.Coordinates.Longitude};
+            if (coords.lat && coords.lng){
+                new google.maps.Marker({
+                    position: coords,
+                    map: map 
+                });
+            }
+        } 
+    }
+    window.initMap = _initMap;
+}(window));
