197d11adfc5be4ca96b11354b90091deab6f1ea4
Nicolas Sam <Nicolas Sam>
2017-02-20 00:57:30 -0500

    Added popups

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-(function(window){
+(function(window, $){
     function _initMap(){
         var map = new google.maps.Map(document.getElementById('map'), {
             zoom: 11,
@@ -13,13 +13,27 @@
 
         function addMarker(point){
             var coords = { lat: point.Coordinates.Latitude, lng:point.Coordinates.Longitude};
+            var content = "<div class='popup'>"+ point.CompanyName +"</div>"
+
+            var infoWindow = new google.maps.InfoWindow({
+                content: content
+            });
             if (coords.lat && coords.lng){
-                new google.maps.Marker({
+                var marker = new google.maps.Marker({
                     position: coords,
                     map: map 
                 });
+
+                marker.addListener('click', function(e){
+                    // map.setZoom(map.getZoom() + 3);
+                    // map.setCenter(marker.position.lat(), marker.position.lng());
+                    // map.setCenter(30, 70);
+                    console.log(marker.position.lat);
+                    infoWindow.open(map, marker)
+                });
             }
+            return marker;
         } 
     }
     window.initMap = _initMap;
-}(window));
+}(window, jQuery));
