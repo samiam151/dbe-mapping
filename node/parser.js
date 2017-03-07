d8ba5e8bfb5eb316b118bbd0bd4e67fe2a7bfba3
Nicolas Sam <Nicolas Sam>
2017-03-07 00:41:29 -0500

    Getting back coreetly fromated business

--- node/parser.js
+++ node/parser.js
@@ -22,32 +22,42 @@ fs.readFile("./python/actual_data.html", (err, file) => {
     trs_business.forEach((tr, i) => {
         arr.push(tr)
         if (everyTwo(i)){
-            arr.push(tr) 
             businesses.push(arr)
             arr = []
         }
     })
 
-
-    let groups = []
-    businesses.forEach((item, index_trs) => {
-        let obj = {}
-        let spans = $(item).find('span') 
-        spans.each((index_span, span) => {
-            // let spans = $(span).find('span')
-            obj[`span_${index_trs}`] = span
-            groups.push(obj)
-        })  
+   
+    businesses = businesses.map((item, index_trs) => {
+        // once(index_trs, () => {
+        //     console.log(item)
+        // })
+``
+        let groups = []
+        item.forEach(td => {
+            let spans = $(td).find('span') 
+            spans.each((index_span, span) => {
+                let obj = {}
+                // let spans = $(span).find('span')
+                obj[span.attribs.id] = $(span).text()
+                groups.push(obj)
+            })
+        })
+        
+        return groups
     })
-    businesses = groups
 
-    let EXAMPLE = businesses[0].span_0;
-    console.log(businesses.length)
-    console.log(EXAMPLE.attribs.id)
-    console.log($(EXAMPLE).text())
+    // let EXAMPLE = businesses[0].span_0;
+    console.log(businesses[0])
+    console.log(businesses[1])
+    console.log(businesses[2])
+    console.log(businesses[3])
+    // console.log(EXAMPLE.attribs.id)
+    // console.log($(EXAMPLE).text())
 })
 
 function everyFour(i){
+    i++
     if ((i - 1) % 4 === 0 || i % 4 === 0){
         return true
     }
@@ -55,8 +65,14 @@ function everyFour(i){
 }
 
 function everyTwo(i){
-    if (i % 2 === 0){
+    if (i % 2 != 0){
         return true
     }
     return false;
+}
+
+function once(i, fn){
+    if (i === 1){
+        fn();
+    }
 }
